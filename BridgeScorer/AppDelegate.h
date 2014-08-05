0a05e433ad5427902f89de8a1cf691a2b05ccd46
Jeff Ching <ching.jeff@gmail.com>
2014-08-04 21:26:03 -0700

    Initial Commit

---
+++ BridgeScorer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BridgeScorer
+//
+//  Created by Jeffrey Ching on 8/4/14.
+//  Copyright (c) 2014 Jeffrey Ching. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BridgeScorer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BridgeScorer
+//
+//  Created by Jeffrey Ching on 8/4/14.
+//  Copyright (c) 2014 Jeffrey Ching. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
